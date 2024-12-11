a9460ac0f97935a0993fc5f2559aeca706dbebda
Carlos Martinez <curlosmart@gmail.com>
2024-12-11 13:35:35 -0500

    Remove unnecessary code.

--- 003.1_Lightwater/Lightwater.py
+++ 003.1_Lightwater/Lightwater.py
@@ -1,7 +1,8 @@
 from gpiozero import LED
 from time import sleep
 
-led_pin_numbers = [17, 18, 27, 22, 23, 24, 25, 2, 3, 8]
+# The schematic diagram from Freenove wanted the 4th LED to be connected to GPIO22 however for some reason this LED would be lit up by default before the code ran. I don't know why and I assume it's left over voltage or something so not 100% sure. In any case, I used GPIO5 instead for the 4th LED (5).
+led_pin_numbers = [17, 18, 27, 5, 23, 24, 25, 2, 3, 8]
 
 # Intialize the LEDs with an intiial value of "True". This will TURN ON all the LEDs by default. This is necessary so that the LEDs DO NOT light up from the start because in the circuit the GPIO pins are connected to the cathode ("-"), not the anode ("+") so it's working through an inverse setup. Meaning for the LEDs to be off by default, we need to set the output to HIGH.
 # Without this, it will intiially be turned "off" (output set to LOW) which will show the LEDs with the light on due to the inverse cathode connection setup.
@@ -11,22 +12,22 @@ def blinkRowOfLEDs():
     '''
     The schematic diagram is designed in such a way that in this circuit, the cathodes ("-", the negative) of the LEDs are connected to the GPIO pins, which is different from the previous circuit where the ANODES ("+") where connected to the GPIO pins. This is the inverse of normal behavior. This means that LEDs turn ON when the GPIO output is set to LOW level in the program.
 
-    - "led.on()" will TURN OFF the LED because that sets it to HIGH level output.
-    - "led.off()" will TURN ON the LED because that sets it to a LOW level output.
+    - IMPORTANT: "led.on()" will TURN OFF the LED because that sets it to HIGH level output.
+    - IMPORTANT: "led.off()" will TURN ON the LED because that sets it to a LOW level output.
     '''
 
     while True:
         # From left to right, turn on and off the individual LEDs.
         for i in range(0, len(leds), 1):
-            leds[i].off() # Set LOW, LED turns ON
+            leds[i].off()
             sleep(0.1)
-            leds[i].on() # Set HIGH, LED turns OFF
+            leds[i].on()
         
         # From right to left, turn on and off the individual LEDs
         for i in range(len(leds) - 1, -1, -1):
-            leds[i].off() # Set LOW, LED turns ON
+            leds[i].off()
             sleep(0.1)
-            leds[i].on() # Set HIGH, LED turns OFF
+            leds[i].on()
 
 is_running_as_main_program = (__name__ == '__main__')
 
@@ -34,9 +35,6 @@ if is_running_as_main_program:
     print('Program is starting...')
 
     try:
-        for led in leds:
-            led.on()
-
         blinkRowOfLEDs()
     except KeyboardInterrupt:
         print('Ending program!')
